9ffe7492deea7d8d99716bfbcffd2c0e0022af7e
devalibello <alibelloimoukhuede@gmail.com>
2023-12-04 20:46:26 +0300

    Add respons_to_on_destroy method to session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -32,6 +32,21 @@ class Users::SessionsController < Devise::SessionsController
       status: :ok
   end
 
-  
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1],
+    Rails.application.credentials.fetch(:secret_key_base)).first
+    current_user = User.find(jwt_payload['sub'])
+    if current_user
+      render json: {
+        status: 200,
+        message: "Signed out successfully"},
+        status: :ok
+    else
+      render json: {
+        status: 401,
+        message: "user has no active session"},
+        status: :unauthorized
+    end
+  end
 
 end
