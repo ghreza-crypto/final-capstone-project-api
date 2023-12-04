d0f3f1161b427192030df361627fda0095ff3427
devalibello <alibelloimoukhuede@gmail.com>
2023-12-04 20:43:55 +0300

    Add create method to sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,4 +3,15 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    if params[:user][:password].present?
+      # Both username and password provided
+      super
+    else
+      # Only username provided, handle accordingly
+      handle_username_authentication
+    end
+  end
+
+  private 
 end
