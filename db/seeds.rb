3ab67ccaca9ae4cb60171a767ccd177f13c62e85
devalibello <alibelloimoukhuede@gmail.com>
2023-12-05 14:32:38 +0300

    Add routes for cars controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user1 = User.find(1)
+car1 = Car.create(
+    user:user1, model: "Honda", description: "A nice car to buy",
+    finance_fee: 10, purchase_fee: 20, amount_payable: 50,
+    duration: "4 Months", apr: "5.8%",
+    photo: "https://www.drivespark.com/images/2023-06/honda-elevate_168604685850.jpg"
+    )
