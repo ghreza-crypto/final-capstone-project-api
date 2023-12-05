f16234bf5dc7538a88fb3fb334b49865f4a90b0f
devalibello <alibelloimoukhuede@gmail.com>
2023-12-05 19:57:50 +0300

    Add destroy action

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -11,4 +11,24 @@ class Api::V1::CarsController < ApiController
         render json: @car, status: :ok
     end
 
+
+
+    def destroy
+        @car = Car.find(params[:id])
+        if @car.destroy
+            render json: { message: "Car deleted successfully", status: "success"
+        }, status: :ok
+        else
+            render json: { message: "Something went wrong", status: "failed"}
+        end
+    end
+
+    private
+
+    def car_params
+        params.require(:car).permit(
+        :photo, :model, :description, :finance_fee,
+        :purchase_fee, :amount_payable, :duration, :apr)
+    end
+
 end
