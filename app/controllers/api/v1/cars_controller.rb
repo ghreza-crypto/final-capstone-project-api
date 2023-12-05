898709c978e216d9492f9cc5a13df70d376fba58
devalibello <alibelloimoukhuede@gmail.com>
2023-12-05 15:19:20 +0300

    Add car show action

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::CarsController < ApiController
+    # before_action :set_car, only: [:show]
     
     def index
         @cars = Car.all
         render json: @cars, status: :ok
     end
+
+    def show
+        @car = Car.find(params[:id])
+        render json: @car, status: :ok
+    end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 #   end
 
 user1 = User.find(1)
-car1 = Car.create(
-    user:user1, model: "Honda", description: "A nice car to buy",
+car2 = Car.create(
+    user:user1, model: "BWM", description: "You cant afford to miss this!",
     finance_fee: 10, purchase_fee: 20, amount_payable: 50,
     duration: "4 Months", apr: "5.8%",
-    photo: "https://www.drivespark.com/images/2023-06/honda-elevate_168604685850.jpg"
+    photo: "https://stimg.cardekho.com/images/carexteriorimages/930x620/BMW/X7/10571/1689673096346/front-left-side-47.jpg?imwidth=890&impolicy=resize"
     )
