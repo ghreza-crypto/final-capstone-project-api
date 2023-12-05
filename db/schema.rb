d38a95a487f0f94915d1a5122a8bfbb13e2513cd
Benson Kiai <benson.kiai86@gmail.com>
2023-12-06 02:54:32 +0300

    migrate reservations

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,39 +10,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_231_205_105_045) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_192228) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'cars', force: :cascade do |t|
-    t.string 'photo'
-    t.string 'model'
-    t.string 'description'
-    t.integer 'finance_fee'
-    t.integer 'purchase_fee'
-    t.integer 'amount_payable'
-    t.string 'duration'
-    t.string 'apr'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_cars_on_user_id'
+  create_table "cars", force: :cascade do |t|
+    t.string "photo"
+    t.string "model"
+    t.string "description"
+    t.integer "finance_fee"
+    t.integer "purchase_fee"
+    t.integer "amount_payable"
+    t.string "duration"
+    t.string "apr"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string 'username'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'jti', null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['jti'], name: 'index_users_on_jti', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "car_id", null: false
+    t.date "date"
+    t.string "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["car_id"], name: "index_reservations_on_car_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
-  add_foreign_key 'cars', 'users'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "jti", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "cars", "users"
+  add_foreign_key "reservations", "cars"
+  add_foreign_key "reservations", "users"
 end
